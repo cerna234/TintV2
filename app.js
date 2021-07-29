79184be61a04bc793b387420ae0fb52724d277e2
cerna234 <51102686+cerna234@users.noreply.github.com>
2021-07-29 10:35:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hh");
+
+function OpenNav(){
+    document.getElementById("box").style.left = "50%";
+
+    console.log("working");
+}
+
+function closeNav(){
+    document.getElementById("box").style.left = "100%";
+}
